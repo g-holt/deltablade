674a534e031d894de77fb98e8b5dbbe874232b90
Grayson Holt <gholtwork@gmail.com>
2022-02-28 18:24:39 -0600

    Setup Initial CharacterSwitcher
    Setup CharacterSwitcher Script so user can choose character from [SerializeField] with 0, 1, or 2

--- DeltaBlade/Assets/Scripts/CharacterSwitcher.cs
+++ DeltaBlade/Assets/Scripts/CharacterSwitcher.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CharacterSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int currentCharacter = 0; 
+
+
     void Start()
     {
-        
+        SetCharacterActive();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
+
+
+    void SetCharacterActive()
+    {
+        int characterIndex = 0;
+
+        foreach(Transform character in transform)
+        {
+            if(characterIndex == currentCharacter)
+            {
+                character.gameObject.SetActive(true);
+            }
+            else
+            {
+                character.gameObject.SetActive(false);
+            }
+            
+            characterIndex++;
+        }
+    }
+
 }
