663e5eb34817a7de0f6d98994cd8635e3bf134da
Grayson Holt <gholtwork@gmail.com>
2022-02-28 23:53:17 -0600

    End of day WeaponSwitcher
    Working on Character/Weapon Switching

--- DeltaBlade/Assets/Scripts/Player/CharacterWeaponSwitcher.cs
+++ DeltaBlade/Assets/Scripts/Player/CharacterWeaponSwitcher.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterSwitcher : MonoBehaviour
+public class CharacterWeaponSwitcher : MonoBehaviour
 {
     public int currentCharacter = 0; 
+
+    CircleCollider2D mainPlayerCollider;
 
 
     void Start()
     {
-        SetCharacterActive();
+        mainPlayerCollider = GetComponentInParent<CircleCollider2D>();
+        SetCharacterActive(currentCharacter);
     }
 
 
-    void Update()
+    public void SetCharacterIndex(int index)
     {
-        
+        SetCharacterActive(index);
     }
 
 
-    void SetCharacterActive()
+    void SetCharacterActive(int currentCharacter)
     {
         int characterIndex = 0;
 
